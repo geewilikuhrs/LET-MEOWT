fb2f0a56488d4ecefd501d0f159ad09ba1403f0e
Katie Lee <klee1215@hotmail.com>
2023-09-27 18:32:08 -0700

    Finished Pause Menu

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -7,20 +7,24 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
         pauseMenu.SetActive(true);
+        Time.timeScale = 0;
     }
 
     public void Home()
     {
         SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1;
     }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
     }
 }
