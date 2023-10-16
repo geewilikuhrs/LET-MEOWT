725cd19bd30b4670e7781a6de2fce9701302751c
Nmenotti <nickymenotti@gmail.com>
2023-10-15 21:12:20 -0700

    some animation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 50f;
     Vector2 lastPos;
     bool moving;
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
@@ -15,12 +16,22 @@ public class PlayerController : MonoBehaviour
             lastPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             moving = true;
         }
-
+        animateMovement();
         if (moving && (Vector2)transform.position != lastPos) {
             float step = speed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, lastPos, step);
         } else {
             moving = false;
+        }
+        
+    }
+    void animateMovement() {
+        if (animator != null) {
+            if(moving) {
+                animator.SetBool("isMoving", true);
+            } else {
+                animator.SetBool("isMoving", false);
+            }
         }
     }
 }
